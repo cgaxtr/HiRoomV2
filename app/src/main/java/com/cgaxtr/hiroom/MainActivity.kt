539a1fa3f83ac83e2d778fa94a9444f68ba904fe
Carlos <cga.xtr@gmail.com>
2022-10-13 20:14:44 +0200

    splash activity

--- app/src/main/java/com/cgaxtr/hiroom/MainActivity.kt
+++ app/src/main/java/com/cgaxtr/hiroom/MainActivity.kt
@@ -3,17 +3,27 @@ package com.cgaxtr.hiroom
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.ViewModelProvider
 import com.cgaxtr.hiroom.ui.screens.LoginScreen
 import com.cgaxtr.hiroom.ui.theme.HiRoomTheme
 import com.cgaxtr.hiroom.ui.viewmodel.LoginViewModel
+import com.cgaxtr.hiroom.ui.viewmodel.SplashViewModel
 
 class MainActivity : ComponentActivity() {
+
+    private val splashViewModel : SplashViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().setKeepOnScreenCondition {
+            splashViewModel.isLoading.value!!
+        }
+
         setContent {
             HiRoomTheme {
                 // A surface container using the 'background' color from the theme
---
+++ app/src/main/java/com/cgaxtr/hiroom/ui/viewmodel/SplashViewModel.kt
@@ -0,0 +1,23 @@
+package com.cgaxtr.hiroom.ui.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class SplashViewModel: ViewModel() {
+
+    private val _isLoading = MutableLiveData(true)
+    val isLoading : LiveData<Boolean> = _isLoading
+
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            //TODO check login and first open
+            Thread.sleep(2000)
+            _isLoading.postValue(false)
+        }
+    }
+
+}
