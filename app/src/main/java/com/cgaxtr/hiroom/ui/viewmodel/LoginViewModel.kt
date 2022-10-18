08346e3cd189a53a7a4c2a010571839cbf8ac035
Carlos <cga.xtr@gmail.com>
2022-10-18 22:25:43 +0200

    some logic added to login viewModel

--- app/src/main/java/com/cgaxtr/hiroom/ui/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/cgaxtr/hiroom/ui/viewmodel/LoginViewModel.kt
@@ -1,17 +1,32 @@
 package com.cgaxtr.hiroom.ui.viewmodel
 
+import android.util.Patterns
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.cgaxtr.hiroom.domain.LoginUseCase
+import kotlinx.coroutines.launch
 
 class LoginViewModel : ViewModel() {
 
+    private companion object {
+        const val MIN_PASSWORD_LENGTH = 8
+    }
 
-    private val _email = MutableLiveData<String>("")
+    val loginUseCase = LoginUseCase()
+
+    private val _email = MutableLiveData("")
     val email: LiveData<String> = _email
 
-    private val _password = MutableLiveData<String>("")
+    private val _password = MutableLiveData("")
     val password: LiveData<String> = _password
+
+    private val _navigateToMain = MutableLiveData(false)
+    val navigateToMain: LiveData<Boolean> = _navigateToMain
+
+    private val _showError = MutableLiveData(false)
+    val showError : LiveData<Boolean> = _showError
 
     fun onTextChanged(email: String, password: String) {
         _email.value = email
@@ -19,7 +34,20 @@ class LoginViewModel : ViewModel() {
     }
 
     fun doLogin() {
-
+        if (isValidEmail(_email.value!!) && isValidPassword(_password.value!!)){
+            viewModelScope.launch {
+                val loggedIn = loginUseCase(_email.value!!, _password.value!!)
+                if(loggedIn)
+                    _navigateToMain.postValue(true)
+                else
+                    _showError.postValue(true)
+            }
+        }
     }
 
+    private fun isValidEmail(email: String) =
+        Patterns.EMAIL_ADDRESS.matcher(email).matches()
+
+
+    private fun isValidPassword(password: String): Boolean = password.length >= MIN_PASSWORD_LENGTH
 }
